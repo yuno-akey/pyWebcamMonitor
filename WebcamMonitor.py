185513535df382496b7f44e53885861841bf2b60
yuno-akey <syurikuzuri@gmail.com>
2024-12-09 15:39:48 +0900

    録画分割機能の試験的実装

--- WebcamMonitor.py
+++ WebcamMonitor.py
@@ -8,11 +8,14 @@ from datetime import datetime, timedelta
 DEFAULT_CONFIG = {
     "VIDEO": {
         "SOURCE": "0",
-        "MOTION_PATH": "motion-detected/"
+        "MOTION_PATH": "motion-detected/",
+        "MAX_DURATION": "1800",  # 秒
+        "MAX_SIZE": "104857600" # 100MBをバイトで表現
     }
 }
 
 def load_config():
+
     config = configparser.ConfigParser()
     config_path = "config.ini"
     motion_path = DEFAULT_CONFIG["VIDEO"]["MOTION_PATH"]
@@ -30,7 +33,7 @@ def load_config():
                 config.write(configfile)
 
     if not config["VIDEO"]["SOURCE"].isdigit() or int(config["VIDEO"]["SOURCE"]) < 0: # ソースが数字でないか0未満の場合はエラー
-        if isinstance(config["VIDEO"]["SOURCE"], str):
+        if isinstance(config["VIDEO"]["SOURCE"], str) and config["VIDEO"]["SOURCE"].endswith(".mp4"): # ソースが文字列で動画ファイルを参照している場合はpass
             pass
         else:
             raise ValueError("Invalid source value in config file")
@@ -46,10 +49,15 @@ def load_config():
 
 
 class VideoWriter:
+
     CONFIG = load_config()
     MOTION_PATH = CONFIG["MOTION_PATH"]
+    MAX_DURATION = int(CONFIG["MAX_DURATION"]) if CONFIG["MAX_DURATION"] else None
+    MAX_SIZE = int(CONFIG["MAX_SIZE"]) if CONFIG["MAX_SIZE"] else None
 
     def __init__(self, width, height, fps, path=None):
+
+        # 引数のチェック処理
         if width <= 0 or height <= 0:
             raise ValueError("Invalid width or height value. Width and height must be positive integers")
         if not isinstance(fps, (int, float)):
@@ -58,29 +66,54 @@ class VideoWriter:
         self.width = width
         self.height = height
         self.fps = max(fps, 1) # fpsが0以下の場合は1に設定
+        self.start_time = datetime.now()
+        self.max_duration = self.MAX_DURATION
+        self.max_size = self.MAX_SIZE
+        self.current_file_index = 0
 
         if path:
-            self.path = os.path.join(self.MOTION_PATH, path) # 動体検知時の保存パス
+            self.base_path = os.path.join(self.MOTION_PATH, path) # 動体検知時の保存パス
         else:
-            self.path = datetime.now().strftime("%Y-%m-%d-%H-%M-%S") + ".mp4" # デフォルトの保存パス
+            self.base_path = datetime.now().strftime("%Y-%m-%d-%H-%M-%S") # デフォルトの保存パス
 
         try:
-            self.codec = cv2.VideoWriter_fourcc(*"h264") #h264形式で保存. mp4vも可.
-            self.writer = cv2.VideoWriter(
-                self.path, 
-                self.codec, 
-                self.fps, 
-                (self.width, self.height), 
-                1 # 1:カラー, 0:グレースケール
-            )
+            self._open_new_writer()
             if not self.writer.isOpened(): # 初期化に失敗した場合は例外を発生
                 raise IOError("Failed to create VideoWriter")
         except Exception as e:
             self.release()
             raise IOError(f"Failed to initialize VideoWriter: {str(e)}")
     
+    def _open_new_writer(self):
+        # 新しいファイル名を生成
+        self.record_path = f"{self.base_path}_part{self.current_file_index}.mp4"
+        self.current_file_index += 1
+
+        # ビデオライターの初期化
+        self.codec = cv2.VideoWriter_fourcc(*"h264") # h264形式で保存
+        self.writer = cv2.VideoWriter(
+            self.record_path,
+            self.codec,
+            self.fps,
+            (self.width, self.height),
+            True
+        )
+        self.start_time = datetime.now()
+
     def write(self, frame):
         self.writer.write(frame)
+
+        # 経過時間のチェック
+        if self.max_duration and (datetime.now() - self.start_time).total_seconds() >= self.max_duration:
+            self._rotate_writer()
+
+        # ファイルサイズのチェック
+        if self.max_size and os.path.getsize(self.record_path) >= self.max_size:
+            self._rotate_writer()
+
+    def _rotate_writer(self):
+        self.release()
+        self._open_new_writer()
     
     def release(self):
         if self.writer:
@@ -104,8 +137,10 @@ class VideoWriter:
 class CaptureVideo:
 
     BOUNDING_COLOR = (0, 0, 255) # 動体検知時のバウンディングボックスの色
+    THRESHOLD = 10 # 二値化の閾値
 
     def __init__(self, source):
+
         self.videoSource = cv2.VideoCapture(source)
         if not self.videoSource.isOpened():
             raise ValueError(f"Failed to open video source: {source}")
@@ -118,19 +153,16 @@ class CaptureVideo:
             self.fps = 30
         self.ACCUMULATED_WEIGHT = 0.6 #移動平均の重み
         self.motion_end_time = None
-        try:
-            self.motion_writer = None
-        except Exception as e:
-            self.release()
-            raise IOError(f"Failed to initialize CaptureVideo: {str(e)}")
+        self.motion_writer = None
 
     def detect_motion(self, frame, gray, before):
+
         if frame is None or gray is None or before is None:
             return False
         try:
             before = cv2.convertScaleAbs(before) # 絶対値を取得
             frame_delta = cv2.absdiff(before, gray)
-            thresh = cv2.threshold(frame_delta, 7, 255, cv2.THRESH_BINARY)[1]
+            thresh = cv2.threshold(frame_delta, self.THRESHOLD, 255, cv2.THRESH_BINARY)[1]
             thresh = cv2.dilate(thresh, None, iterations=2)
             contours, _ = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
@@ -148,6 +180,7 @@ class CaptureVideo:
             return False
 
     def showVideo(self):
+
         try:
             before = None # 前フレーム。初期値はNone
             with VideoWriter(self.width, self.height, self.fps) as default_writer:
